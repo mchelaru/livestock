cd11b0a4ad0d8221e696b28a212a509977692461
Mihai Chelaru <mihaiche@yahoo.com>
2024-10-26 12:40:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# LIVESTOCK
+
+Livestock is a portfolio value tracker. Usage:
+
+```
+livestock [OPTIONS] --file <FILE>
+
+Options:
+  -f, --file <FILE>  The JSON configuration file
+      --days <DAYS>  The number of days to look back [default: 10]
+      --debug        Prints additional debug information
+  -h, --help         Print help
+  -V, --version      Print version
+```
+
+## Configuration file example
+
+```
+{
+    "Stocks": {
+        "AAPL": 50,
+        "CSCO": 20,
+        "SPY": 25,
+        "TSLA": 10
+    }
+}
+```
+
+## Examples
+
+Running it on the file above returns:
+
+```./livestock -f stocks.json --days 5
+Querying Yahoo! Finance...
+Portfolio evolution on the last 5 days
+⡁                             ⢰⠉⠉⠉⠉⠉⠉⠁ 29853.2
+⠍⠉⠉⠉⠉⠉⠉⠉⠒⠒⠒⠒⠒⠒⠒⡆      ⢰⠉⠉⠉⠉⠉          
+⠂                ⡇      ⢸               
+⡁                ⡇      ⢸               
+⠄                ⡇      ⢸               
+                 ⠈⠉⠉⠉⠉⠉⠉⠉              29249.8
+
+
+Portfolio total value: 29853.20
+```
---
+++ src/main.rs
@@ -0,0 +1,143 @@
+use clap::Parser;
+use std::{collections::HashMap, fs};
+use textplots::{Chart, LabelBuilder, Plot, Shape};
+use tokio_test;
+use yahoo_finance_api::{self as yf, Quote};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The JSON configuration file
+    #[arg(short, long)]
+    file: String,
+
+    /// The number of days to look back
+    #[arg(long, default_value_t = 10)]
+    days: usize,
+
+    /// Prints additional debug information
+    #[arg(long, default_value_t = false)]
+    debug: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    // read the symbols
+    let file = fs::File::open(args.file).expect("file should open read only");
+    let json: serde_json::Value =
+        serde_json::from_reader(file).expect("file should be proper JSON");
+    let stocks_str = json
+        .get("Stocks")
+        .expect("config file should have Stocks key");
+    let stocks_dict: HashMap<String, u32> = serde_json::from_value(stocks_str.clone()).unwrap();
+
+    let mut quotes: HashMap<String, Vec<Quote>> = HashMap::new();
+    let provider = yf::YahooConnector::new().unwrap();
+
+    println!("Querying Yahoo! Finance...");
+    for (ticker, quantity) in &stocks_dict {
+        // resolve the symbols
+        let yahoo_symbol;
+        match tokio_test::block_on(provider.search_ticker(&ticker)) {
+            Ok(r) => {
+                if r.quotes.len() < 1 {
+                    eprintln!("Error matching symbol {ticker}");
+                    continue;
+                } else if r.quotes.len() > 1 && args.debug {
+                    eprintln!("Multiple matches for {ticker} - using the first match");
+                    println!(
+                        "{}",
+                        r.quotes
+                            .iter()
+                            .map(|q| q.symbol.clone())
+                            .reduce(|mut acc, s| {
+                                acc.push_str(" ");
+                                acc.push_str(&s);
+                                acc
+                            })
+                            .unwrap()
+                    );
+                }
+                yahoo_symbol = r.quotes[0].symbol.clone();
+            }
+            Err(_) => {
+                eprintln!("Error searching for symbol {ticker}");
+                continue;
+            }
+        }
+
+        let response = tokio_test::block_on(provider.get_quote_range(
+            &yahoo_symbol,
+            "1d",
+            &format!("{}d", args.days),
+        ));
+        match response {
+            Ok(response) => {
+                let q = response.quotes().unwrap();
+                quotes.insert(ticker.clone(), q);
+
+                if args.debug {
+                    let quote_at_close = response.last_quote().unwrap().close;
+                    println!(
+                        "Last quote at close for {yahoo_symbol}: {} * {} = {}",
+                        quote_at_close,
+                        *quantity,
+                        quote_at_close * (*quantity) as f64
+                    );
+                }
+            }
+            Err(_) => {
+                eprintln!("Error loading quotes for symbol {ticker}");
+                continue;
+            }
+        }
+    }
+
+    // graph and print the total value
+    let mut values = Vec::with_capacity(args.days);
+    for day in 0..args.days {
+        let mut value = 0.;
+        for (ticker, quantity) in &stocks_dict {
+            let ticker_quote = quotes.get(ticker);
+            match ticker_quote {
+                Some(q) => match q.get(day) {
+                    Some(quote) => value += quote.close * (*quantity) as f64,
+                    None => println!("Error accessing day {day} for ticker {ticker}"),
+                },
+                None => {
+                    eprintln!("No value found for {ticker}");
+                }
+            }
+        }
+        values.push(value);
+    }
+    if args.days > 1 {
+        println!("Portfolio evolution on the last {} days", args.days);
+        Chart::new(75, 20, 0., values.len() as f32 - 0.1)
+            .x_label_format(textplots::LabelFormat::None)
+            .lineplot(&Shape::Continuous(Box::new(|i| values[i as usize] as f32)))
+            .display();
+    }
+    println!("Portfolio total value: {:.2}", values[values.len() - 1]);
+}
+
+#[cfg(test)]
+mod test {
+    use textplots::{Chart, Plot, Shape};
+    use tokio_test;
+    use yahoo_finance_api as yf;
+
+    #[test]
+    fn something() {
+        let provider = yf::YahooConnector::new().unwrap();
+
+        let response = tokio_test::block_on(provider.get_quote_range("AAPL", "1d", "10d")).unwrap();
+        let v = response.quotes().unwrap();
+        println!("{v:#?}");
+
+        Chart::new(75, 20, 0., v.len() as f32 - 0.5)
+            .lineplot(&Shape::Continuous(Box::new(|i| v[i as usize].close as f32)))
+            .display();
+    }
+}
